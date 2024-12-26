7587acb692bad4c8a4a80f08e18a75182509136a
Souvik Das <souvikdas.dev@gmail.com>
2024-12-27 01:32:28 +0530

    update some components

--- frontend/components/DropDown.js
+++ frontend/components/DropDown.js
@@ -40,7 +40,6 @@ export default function DropDown({
   if (width == "48") width = "w-48";
 
   const toggleIsOpen = () => {
-    console.log("isOpen", isOpen);
     setIsOpen((open) => !open);
   };
 
--- frontend/components/InputLabel.js
+++ frontend/components/InputLabel.js
@@ -1,9 +1,16 @@
 import { clsx } from "clsx";
 
-export default function InputLabel({ children, value, className, props }) {
+export default function InputLabel({
+  children,
+  htmlFor,
+  value,
+  className,
+  props,
+}) {
   return (
     <label
       className={clsx("block font-medium text-sm text-gray-700", className)}
+      htmlFor={htmlFor}
       {...props}
     >
       {value ?? children}
