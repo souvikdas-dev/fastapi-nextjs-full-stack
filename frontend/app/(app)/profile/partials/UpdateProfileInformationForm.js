8e0df3e7aa91939b4679f6d2a89951964d7f9500
Souvik Das <souvikdas.dev@gmail.com>
2025-01-10 17:05:54 +0530

    added profile update functionality

--- frontend/app/(app)/profile/partials/UpdateProfileInformationForm.js
+++ frontend/app/(app)/profile/partials/UpdateProfileInformationForm.js
@@ -3,14 +3,36 @@ import { updateProfile } from "@/app/_actions/profile";
 import PrimaryButton from "@/components/buttons/PrimaryButton";
 import InputError from "@/components/InputError";
 import InputLabel from "@/components/InputLabel";
+import ServerErrors from "@/components/ServerErrors";
 import TextInput from "@/components/TextInput";
-import { useActionState } from "react";
+import { useActionState, useEffect } from "react";
+import toast, { Toaster } from "react-hot-toast";
 
-export default function UpdateProfileInformationForm(params) {
+export default function UpdateProfileInformationForm({ ...props }) {
   const [state, action, pending] = useActionState(updateProfile, undefined);
+
+  useEffect(() => {
+    if (state?.status) {
+      toast.success(<b>{state.message}</b>, { id: "profileInfoToastId" });
+    } else if (state?.errors) {
+      toast.error(<b>Failed to save Profile Information.</b>, {
+        id: "profileInfoToastId",
+      });
+    }
+
+    if (pending) {
+      toast.loading("Saving Profile Information...", {
+        id: "profileInfoToastId",
+      });
+    }
+
+    return () => toast.dismiss("profileInfoToastId");
+  }, [state, pending]);
 
   return (
     <section>
+      <Toaster position="bottom-center" />
+
       <header>
         <h2 className="text-lg font-medium text-gray-900">
           Profile Information
@@ -32,6 +54,7 @@ export default function UpdateProfileInformationForm(params) {
             required
             autoFocus
             autoComplete="name"
+            defaultValue={state?.formData?.name}
           />
           <InputError className="mt-2" messages={state?.errors?.name} />
         </div>
@@ -45,24 +68,20 @@ export default function UpdateProfileInformationForm(params) {
             className="block w-full mt-1"
             required
             autoComplete="username"
+            defaultValue={state?.formData?.email}
           />
           <InputError className="mt-2" messages={state?.errors?.email} />
         </div>
+
+        {/* server errors */}
+        {state?.errors?.server_errors && (
+          <ServerErrors messages={state?.errors?.server_errors} />
+        )}
 
         <div className="flex items-center gap-4">
           <PrimaryButton disabled={pending}>
             {pending ? "Saving..." : "Save"}
           </PrimaryButton>
-
-          {/* @if (session('status') === 'profile-updated')
-                <p
-                    x-data="{ show: true }"
-                    x-show="show"
-                    x-transition
-                    x-init="setTimeout(() => show = false, 2000)"
-                    className="text-sm text-gray-600"
-                >Saved.</p>
-            @endif */}
         </div>
       </form>
     </section>
--- frontend/app/_actions/profile.js
+++ frontend/app/_actions/profile.js
@@ -1,51 +1,80 @@
 "use server";
 
-import { extractFormFields, formatFieldErrors } from "@/utils";
+import { formatFieldErrors } from "@/utils";
 import axios from "axios";
+import { getAccessToken } from "../_lib/session";
 
 export async function updateProfile(state, formData) {
-  let response_data = "";
+  //** wait for 6 seconds */
+  await new Promise((resolve) => setTimeout(resolve, 1500));
 
-  try {
-    const { data } = await axios.patch(
-      process.env.BACKEND_API_URL + "/profile",
-      formData,
-      {
-        headers: {
-          "Content-Type": "application/x-www-form-urlencoded",
-        },
+  const access_token = await getAccessToken();
+
+  const form_fields = {
+    name: formData.get("name"),
+    email: formData.get("email"),
+  };
+
+  return await axios
+    .patch(process.env.BACKEND_API_URL + "/profile", form_fields, {
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded",
+        Authorization: `Bearer ${access_token}`,
+        // Authorization:
+        //   "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoxLCJleHAiOjE3MzY0MDI5NTB9.nCdclB2CpRY5LvPlBSgQO3dc2cg-NnPUQdkD1HbBZ6I",
+      },
+    })
+    .then(function (response) {
+      // console.log(response);
+
+      return {
+        formData: form_fields,
+        status: "profile-updated",
+        message: "Profile updated successfully",
+      };
+    })
+    .catch(function (error) {
+      // console.log(error);
+
+      let errors = {};
+
+      if (error.response) {
+        // The request was made and the server responded with a status code
+        // that falls out of the range of 2xx
+        const response = error.response;
+        // console.log(response);
+
+        if (response.status === 422) {
+          // returning object
+          errors = formatFieldErrors(response.data.detail);
+        } else {
+          errors = {
+            server_errors: [response.data.detail],
+          };
+        }
+      } else if (error.request) {
+        // The request was made but no response was received
+        // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
+        console.error(
+          "Unable to connect to the backend. Please try again later."
+        );
+        errors = {
+          server_errors: ["Something went wrong. Please try again later."],
+        };
+      } else {
+        // Something happened in setting up the request that triggered an Error
+        console.error("Error", error.message);
+
+        errors = {
+          server_errors: ["Something went wrong. Please try again later."],
+        };
       }
-    );
-
-    response_data = data;
-  } catch (error) {
-    if (error.response) {
-      // The request was made and the server responded with a status code
-      // that falls out of the range of 2xx
-      const response = error.response;
-      let errors = error.response.data.detail;
-
-      if (error.response.status === 422) errors = formatFieldErrors(errors);
 
       return {
-        formData: extractFormFields(formData, ["name", "email"]),
-        serverErrors: [errors],
+        formData: form_fields,
+        errors: errors,
       };
-    } else if (error.request) {
-      // The request was made but no response was received
-      // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
-      console.error(
-        "Unable to connect to the backend. Please try again later."
-      );
-      return {
-        formData: extractFormFields(formData, ["name", "email"]),
-        serverErrors: ["Unable to connect to server. Please try again later."],
-      };
-    } else {
-      // Something happened in setting up the request that triggered an Error
-      console.error("Error", error.message);
-    }
-  }
+    });
 }
 
 export async function updatePassword(params) {
